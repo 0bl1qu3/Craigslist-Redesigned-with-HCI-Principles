065caa17c5d53b7b90bcadddba6f9feb54fcf3b3
Sandile <stl9970080@gmail.com>
2025-08-29 14:37:15 +0200

    Updated from HashRouter to BrowserRouter

--- craigslistRedesigned/src/App.jsx
+++ craigslistRedesigned/src/App.jsx
@@ -1,24 +1,22 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './Screens/HomePage';
 import CategoryPage from "./Screens/CategoryPage";
-
 import { MantineProvider } from "@mantine/core";
 
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 
 function App() {
-
   return (
-   <MantineProvider>
-     <HashRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/category/:name" element={<CategoryPage />} />
-      </Routes>
-    </HashRouter>
-   </MantineProvider>
-  )
+    <MantineProvider>
+      <BrowserRouter basename="/Craigslist-Redesigned-with-HCI-Principles/">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/category/:name" element={<CategoryPage />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+  );
 }
 
-export default App
+export default App;
